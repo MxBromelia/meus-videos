5cb53ba6c0ffa0d2faff893b27bb5ac763cc8cdf
Ruan Macambira do Nascimento <ruanmacambira@gmail.com>
2021-08-15 17:57:40 -0300

    Change Categories Index to Show All Categories Instead of Only Bookmarked ones

--- spec/features/categories/categories_spec.rb
+++ spec/features/categories/categories_spec.rb
@@ -1,19 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Listing Categories", type: :feature do
+  let!(:category) { create :category }
+
   feature "Listing Categories" do
     background do
-      create :category, bookmarked: true, name: "Favoritado"
-      create :category, bookmarked: false, name: "Não Favoritado"
       visit 'categories/'
     end
 
-    scenario "Bookmarked Categories Show in Index" do
-      expect(page).to have_content("Favoritado")
-    end
-
-    scenario "Unbookmarked Categories Show in Index" do
-      expect(page).to have_content("Não Favoritado")
+    scenario "Show All Categories in Index" do
+      expect(page).to have_content(category.name)
     end
   end
 end
